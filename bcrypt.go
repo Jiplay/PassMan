68745c0f1b4ba3c2b3bf9633e38eccf377392e39
Julien Garsia <julien.garsia@epitech.eu>
2024-03-30 18:15:34 +0100

    🚧 Encryption of subsidiaries password

--- bcrypt.go
+++ bcrypt.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
 	"golang.org/x/crypto/bcrypt"
+	"io"
+	"math/big"
 	"unicode"
 )
 
@@ -53,4 +60,64 @@ func isPswStrongEnough(password string) bool {
 	}
 
 	return true
+}
+
+func GeneratePassword(length int) (string, error) {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=[]{}|;:,.<>?/~"
+
+	password := make([]byte, length)
+	charsetLen := big.NewInt(int64(len(charset)))
+
+	for i := 0; i < length; i++ {
+		randomIndex, err := rand.Int(rand.Reader, charsetLen)
+		if err != nil {
+			return "", err
+		}
+		password[i] = charset[randomIndex.Int64()]
+	}
+
+	return string(password), nil
+}
+
+func Encrypt(key, text string) (string, error) {
+	hashedKey := sha256.Sum256([]byte(key))
+	block, err := aes.NewCipher(hashedKey[:])
+	if err != nil {
+		return "", err
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(text))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	cfb := cipher.NewCFBEncrypter(block, iv)
+	cfb.XORKeyStream(ciphertext[aes.BlockSize:], []byte(text))
+	return base64.URLEncoding.EncodeToString(ciphertext), nil
+}
+
+func Decrypt(key, text string) (string, error) {
+	hashedKey := sha256.Sum256([]byte(key))
+	block, err := aes.NewCipher(hashedKey[:])
+	if err != nil {
+		return "", err
+	}
+
+	ciphertext, err := base64.URLEncoding.DecodeString(text)
+	if err != nil {
+		return "", err
+	}
+
+	if len(ciphertext) < aes.BlockSize {
+		return "", fmt.Errorf("ciphertext too short")
+	}
+
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	cfb := cipher.NewCFBDecrypter(block, iv)
+	cfb.XORKeyStream(ciphertext, ciphertext)
+
+	return string(ciphertext), nil
 }
